78b7c76aa7fec32e20827371825111e11bc4c6ca
levmanuel <manuel.leveque@gmail.com>
2025-02-09 21:04:10 +0100

    OK

--- dags/diabetes_training_dag.py
+++ dags/diabetes_training_dag.py
@@ -12,7 +12,6 @@ def train_model():
     X_train, X_test, y_train, y_test = train_test_split(
         diabetes.data, diabetes.target, test_size=0.2, random_state=42
     )
-    
     # Configurer MLflow
     mlflow.set_experiment("diabetes_regression")
     with mlflow.start_run():
